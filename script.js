224a321c05c98a00da79e0fee882ef920e1ff3a5
Deepanshu9643 <Deepanshukhorwal7@gmail.com>
2024-09-12 22:43:34 +0530

    new change

--- script.js
+++ script.js
@@ -1,51 +1,50 @@
-
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
-const emailErr = document.querySelector('#emailError');
-const passwordErr = document.querySelector('#passwordError');
-const successMsg = document.querySelector('#successMessage');
+const emailErr = document.querySelector('#emailErr');
+const passwordErr = document.querySelector('#passwordErr');
+const formSuccess = document.querySelector('#formSuccess');
 
 email.addEventListener('input', validateEmail);
 password.addEventListener('input', validatePassword);
 
 function validateEmail() {
-    const emailVal = email.value;
-    if (emailVal.length > 3 && emailVal.includes('@') && emailVal.includes('.')) {
+    const emailValue = email.value;
+    if (emailValue.length > 3 && emailValue.includes('@') && emailValue.includes('.')) {
         emailErr.style.display = 'none';
-        checkAllValid();
+        checkFormStatus();
     } else {
         emailErr.style.display = 'inline';
-        successMsg.style.display = 'none';
+        formSuccess.style.display = 'none';
     }
 }
 
 function validatePassword() {
-    const passwordVal = password.value;
-    if (passwordVal.length > 8) {
+    const passwordValue = password.value;
+    if (passwordValue.length > 8) {
         passwordErr.style.display = 'none';
-        checkAllValid();
+        checkFormStatus();
     } else {
         passwordErr.style.display = 'inline';
-        successMsg.style.display = 'none';
+        formSuccess.style.display = 'none';
     }
 }
 
-function checkAllValid() {
+function checkFormStatus() {
     if (emailErr.style.display === 'none' && passwordErr.style.display === 'none') {
-        successMsg.style.display = 'inline';
+        formSuccess.style.display = 'inline';
     }
 }
 
 function submitForm() {
-    if (successMsg.style.display === 'inline') {
-        if (confirm('Are you sure you want to submit?')) {
-            alert('Successful signup!');
+    if (formSuccess.style.display === 'inline') {
+        if (confirm('Do you want to submit the form?')) {
+            alert('Signup successful!');
         } else {
             email.value = '';
             password.value = '';
             emailErr.style.display = 'inline';
             passwordErr.style.display = 'inline';
-            successMsg.style.display = 'none';
+            formSuccess.style.display = 'none';
         }
     }
 }
